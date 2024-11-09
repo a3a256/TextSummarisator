18d304c4a848dbc10e35b187ac9eeca81d68ecdc
a3a256 <81564718+a3a256@users.noreply.github.com>
2024-11-09 23:47:24 +0000

    added authentication of huggingface token

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ class UI:
         self.ui = ui
         self.first = 0
         device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.summarizer = pipeline("summarization", model="text_summarization", device=device)
+        # self.summarizer = pipeline("summarization", model="text_summarization", device=device)
         self.is_start = False
 
     def test(self, txt):
@@ -22,13 +22,37 @@ class UI:
         res = self.summarizer(txt, max_length=1000, min_length=30, do_sample=False)[0]["summary_text"]
         textfield.insert(END, res)
 
-    def go(self):
-        lb = Label(master=self.ui, text="Enter the text to summarise")
-        lb.grid(row=0, column=0)
-        textfield = ScrolledText(self.ui, wrap=WORD)
-        textfield.grid(row=1, column=0)
-        btn = Button(master=self.ui, text='Review', command=lambda: self.test(textfield.get("1.0", END)))
+    def entering_hf_token(self):
+        hf_lb = Label(master=self.ui, text="Enter you Huggingface token:")
+        hf_lb.grid(row=0, column=0)
+
+        hf_ent = Entry(master=self.ui)
+        hf_ent.grid(row=1, column=0)
+
+        btn = Button(master=self.ui, text="Enter")
         btn.grid(row=2, column=0)
+
+    def go(self):
+        lb = Label(master=self.ui, text="Do you have Huggingface token?")
+        lb.grid(row=0, column=0)
+        def no_hf_token():
+            no_button.destroy()
+            yes_button.destroy()
+            lb.config(text="Please register on Huggingface and generate token")
+
+        def yes_hf_token():
+            yes_button.destroy()
+            no_hf_token()
+            lb.destroy()
+            self.entering_hf_token()
+        yes_button = Button(master=self.ui, text="Yes", command=yes_hf_token)
+        yes_button.grid(row=1, column=0)
+        no_button = Button(master=self.ui, text="No", command=no_hf_token)
+        no_button.grid(row=2, column=0)
+        # textfield = ScrolledText(self.ui, wrap=WORD)
+        # textfield.grid(row=1, column=0)
+        # btn = Button(master=self.ui, text='Review', command=lambda: self.test(textfield.get("1.0", END)))
+        # btn.grid(row=2, column=0)
         self.ui.mainloop()
 
 
